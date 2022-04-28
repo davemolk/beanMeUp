b30f92e275dc73f1a8e29a3f13cb5ef0a188517e
davemolk <davemolk@gmail.com>
2022-04-28 06:03:10 -0600

    add email

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/davemolk/goRGWaitList
 
 go 1.18
 
-require github.com/PuerkitoBio/goquery v1.8.0
+require (
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/joho/godotenv v1.4.0
+)
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"net/smtp"
+	"os"
+	s "strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/joho/godotenv"
 )
 
+type Beans map[string]bool
 
 func main() {
 	beanUrl := "https://www.ranchogordo.com/collections/out-of-stock-beans"
@@ -34,13 +40,120 @@ func main() {
 		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 
+	// scrape phase
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Fatal("error parsing Body", err)
 	}
 
+	today := Beans{}
+
 	doc.Find("div.sold-out").Each(func(i int, s *goquery.Selection) {
 		name := s.Find("p.grid-link__title").First().Text()
-		fmt.Println(name)
+		if name == "" {
+			message := []byte("issue with scrape -- please check selectors")
+			email(message)
+		}
+		today[name] = true
 	})
+
+	
+
+	// compare
+	yesterday := Beans{
+		"Black Garbanzo Bean":      true,
+		"Very tasty bean": true,
+		"Cassoulet (Tarbais) Bean": true,
+		"Chiapas Black Bean":       true,
+		"The best": true,
+	}
+
+	available := []string{}
+
+	for name := range yesterday {
+		if _, ok := today[name]; !ok {
+			available = append(available, name)
+		}
+	}
+
+	fmt.Println(available)
+
+	// convert to json for upload
+	js, err := json.MarshalIndent(today, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(js))
+
+	// email results [CURRENTLY NOT THE CORRECT DATA]
+	message := []byte(string(js))
+	email(message)
+
+	// get yesterday data
+	storage := Beans{}
+	if err := json.Unmarshal(js, &storage); err != nil {
+		log.Fatal("cannot unmarshal", err)
+	}
+	fmt.Println("********************************")
+	fmt.Println(storage)
+	
+
+	
 }
+
+func email(message []byte) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env", err)
+	}
+
+	from := os.Getenv("FROM")
+	password := os.Getenv("PASSWORD")
+	to := []string{
+		os.Getenv("TO"),
+	}
+	smtpHost := os.Getenv("SMTP_HOST")
+	smtpPort := os.Getenv("SMTP_PORT")
+
+	body := message
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	addr := s.Join([]string{smtpHost, smtpPort}, ":")
+
+	emailErr := smtp.SendMail(addr, auth, from,to, body)
+	if emailErr != nil {
+		log.Println("unable to send email", emailErr)
+		return
+	}
+	log.Printf("Email sent to %s", to[0])
+}
+/*
+
+package main
+
+import (
+	"fmt"
+)
+
+type Beans map[string]bool
+
+func main() {
+	old := Beans{
+		"Black Garbanzo Bean":      true,
+		"Cassoulet (Tarbais) Bean": true,
+	}
+	new := []string{"Black Garbanzo Bean", "Cassoulet (Tarbais) Bean", "Chiapas Black Bean"}
+
+	result := []string{}
+
+	for _, v := range new {
+		if _, ok := old[v]; !ok {
+			result = append(result, v)
+		}
+	}
+	fmt.Println(result)
+
+}
+*/
