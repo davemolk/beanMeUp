bbc05b37bea6ec620e8ec1a16ced510fd83e0cad
davemolk <davemolk@gmail.com>
2022-04-29 20:47:53 -0600

    makeRequest function

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ import (
 type Beans map[string]bool
 
 const beanUrl = "https://www.ranchogordo.com/collections/out-of-stock-beans"
+const uAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
 
 type Weekday int
 
@@ -38,27 +39,12 @@ const (
 )
 
 func main() {
-	uAgent := "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
-
-	client := &http.Client{
-		Timeout: 10 * time.Second,
-	}
-
-	req, err := http.NewRequest("GET", beanUrl, nil)
-	if err != nil {
-		log.Fatal("unable to set request", err)
-	}
-	req.Header.Set("User-Agent", uAgent)
-
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatal("request failed", err)
-	}
+	
+	res := makeRequest()
+	
 	defer res.Body.Close()
 
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
+	
 
 	// scrape phase
 	doc, err := goquery.NewDocumentFromReader(res.Body)
@@ -156,6 +142,29 @@ func main() {
 		message := append(intro, byteBeans...)
 		email(message)
 	}	
+}
+
+func makeRequest() *http.Response {
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	req, err := http.NewRequest("GET", beanUrl, nil)
+	if err != nil {
+		log.Fatal("unable to set request", err)
+	}
+	req.Header.Set("User-Agent", uAgent)
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal("request failed", err)
+	}
+
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	return res
 }
 
 func key() (string, string, error) {
